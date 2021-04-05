7e760f8593ad0cad818b9b4981b4f454d7089b9a
Leonichel <leonichel@pop-os.localdomain>
2021-04-05 14:40:39 -0300

    Model predictions add

--- setup.sh
+++ setup.sh
@@ -10,4 +10,10 @@ echo "\
 headless = true\n\
 enableCORS=false\n\
 port = $PORT\n\
+[theme]\n\
+primaryColor='#ff5555'\n\
+backgroundColor='#282a36'\n\
+secondaryBackgroundColor='#44475a'\n\
+textColor='#f8f8f2'\n\
+font='sans serif'\n\
 " > ~/.streamlit/config.toml
--- trade.py
+++ trade.py
@@ -1,72 +1,111 @@
-# bibliotecas
 import streamlit as st
 import requests_cache
-
 import pandas as pd
 import numpy as np
-
 import yfinance as yf
 from stockstats import StockDataFrame as stockDF
-
+from sklearn.neighbors import KNeighborsRegressor
+from sktime.forecasting.compose import ReducedForecaster
 from plotly.subplots import make_subplots
 import plotly.graph_objects as go
 import plotly.express as px 
 
-# Configurações
+Periods = ('1mo','3mo','6mo','1y','2y','5y','10y','ytd','max')
 
-# sessões de cache
-session = requests_cache.CachedSession('yfinance.cache')
-session.headers['User-agent'] = 'my-program/1.0'
+def get_cache_session():
+  session = requests_cache.CachedSession('yfinance.cache')
+  session.headers['User-agent'] = 'my-program/1.0'
 
-# nome da aplicação
-st.title('TradePy')
+  return session
 
-# configurando sidebar
-st.sidebar.header('Escolher ação')
-choice = st.sidebar.text_input('Digite um ticker válido: ')
-delta = st.sidebar.radio('Escolha o período: ', 
-  ('5d','1mo','3mo','6mo','1y','2y','5y','10y','ytd','max'))
+def get_user_choices():
+  st.sidebar.header('Escolher ação')
+  ticker_choice = st.sidebar.text_input('Digite um ticker válido: ')
+  period_choice = st.sidebar.radio('Escolha o período: ', Periods)
 
-# verificando se ação foi selecionada
-if not choice:
-  st.stop()
+  return ticker_choice, period_choice
 
-# ler ação selecionada
 @st.cache(allow_output_mutation=True)
-def get_data():
-  ticker = yf.Ticker(choice, session=session)
-  return ticker
+def get_data(ticker_choice, period_choice, session):
+  ticker_data = yf.Ticker(ticker_choice, session=session)
+  df = pd.DataFrame(ticker_data.history(period=period_choice))
+  stock = stockDF.retype(df)
+  df['macd'] = stock['macd']
 
-ticker = get_data()
-df = pd.DataFrame(ticker.history(period=delta))
+  return df
 
-# obter o macd
-stock = stockDF.retype(df)
-df['macd'] = stock['macd']
+def get_ticker_plot(df):
+  ticker_plot = px.line(df, x=df.index, y=df.columns[1:4])
 
-# Display
+  return ticker_plot
 
-# ação escolhida
-st.write('AÇÃO: ', choice)
+def get_macd_plot(df):
+  macd_plot = make_subplots(vertical_spacing=0, rows=2, cols=1, 
+    row_heights=[4,3])
+  macd_plot.add_trace(go.Candlestick(x=df.index, name='Candlestick', 
+    open=df['open'], high=df['high'], low=df['low'], close=df['close']), 
+    row=1, col=1)
+  macd_plot.add_trace(go.Scatter(x=df.index, name='MACD', 
+    line=dict(color='blue')), row=2, col=1)
+  macd_plot.update_layout(xaxis_rangeslider_visible=False, 
+    xaxis=dict(zerolinecolor='black', showticklabels=False),
+    xaxis2=dict(showticklabels=False))
+  macd_plot.update_xaxes(showline=True, linewidth=1, linecolor='black', 
+    mirror=False)
 
-# overview
-st.markdown('## Overview')
-over = px.line(df, x=df.index, y=df.columns[1:4])
-st.plotly_chart(over)
+  return macd_plot
 
-# macd
-st.markdown('## MACD')
+def get_predictions_data(df):
+  y = pd.Series(df['close'], index=df.index)
+  r = pd.date_range(start=y.index.min(), end=y.index.max())
+  y = y.reindex(r).fillna(0.0)
+  y = y.replace(to_replace=0.0, method='ffill')
 
-macd = make_subplots(vertical_spacing=0, rows=2, cols=1, row_heights=[4,3])
+  regressor = KNeighborsRegressor(n_neighbors=1)
+  forecaster = ReducedForecaster(
+    regressor, scitype="regressor", window_length=15, strategy="recursive")
+  forecaster.fit(y)
 
-macd.add_trace(go.Candlestick(x=df.index, name='Candlestick', open=df['open'], 
-  high=df['high'], low=df['low'], close=df['close']), row=1, col=1)
-macd.add_trace(go.Scatter(x=df.index, name='MACD', line=dict(color='blue')), 
-  row=2, col=1)
+  fh = np.arange(10)
+  prediction = forecaster.predict(fh)
+  y_pred = pd.concat([y, prediction])
 
-macd.update_layout(xaxis_rangeslider_visible=False, 
-  xaxis=dict(zerolinecolor='black', showticklabels=False),
-  xaxis2=dict(showticklabels=False))
-macd.update_xaxes(showline=True, linewidth=1, linecolor='black', mirror=False)
+  predictions_data = pd.DataFrame({ 'Real': y, 'Prediction': prediction}, 
+    index=y_pred.index)
 
-st.plotly_chart(macd)
+  return predictions_data
+
+def get_predictions_plot(predictions_data):
+  predictions_plot = px.line(predictions_data.tail(60), 
+    x=predictions_data.tail(60).index, y=predictions_data.tail(60).columns)
+
+  return predictions_plot
+
+ticker_choice, period_choice = get_user_choices()
+
+if st.sidebar.button('Ler ticker'):
+
+  session = get_cache_session()
+  df = get_data(ticker_choice, period_choice, session)
+
+  st.write('# TradePy')
+  st.write('AÇÃO: ', ticker_choice)
+
+  st.write('## Overview')
+  st.write('Plote da visão geral do ticker.')
+  ticker_plot = get_ticker_plot(df)
+  st.plotly_chart(ticker_plot)
+
+  st.write('## Indicador MACD')
+  st.write('Indicador de análise técnica do mercado financeiro.')
+  macd_plot = get_macd_plot(df)
+  st.plotly_chart(macd_plot)
+
+  st.markdown('## Predições')
+  st.write('Predição para os próximos 10 dias.')
+  predictions_data = get_predictions_data(df)
+  predictions_plot = get_predictions_plot(predictions_data)
+  st.plotly_chart(predictions_plot)
+
+else:
+  st.stop()
